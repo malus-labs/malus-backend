0b8ca3fb564f9af5de968e693f352432947f5a05
Warren Simpson <warren.simpson34@gmail.com>
2020-09-22 16:30:35 -0400

    Fixed name of function from getUserStoreId to getUserStore in the usefull_functions/getUserStore, inside routes/user_store passed fetchUserId to getUserStore instead of user_id, fixed the User.fallAll function in getUserStore to get the right limit.

--- routes/user_store.js
+++ routes/user_store.js
@@ -13,18 +13,18 @@ router.get("/:user_id/:pageNumber", async (req, res, next) => {
         
         if(user_id.substring(0, 2) == "0x") {
             const fetchUserId = await getUserId(user_id);
-            if(fetchUserId == - 1) { //fixed tommarow..
+            if(fetchUserId == - 1) { 
                 userStores = [
                     {"amount_pages_left": 0},
                     {"page_number": convertedPageNumber}
                 ]
             }
             else {
-                userStores = await getUserStore(user_id, convertedPageNumber);
+                userStores = await getUserStore(fetchUserId, convertedPageNumber);
             }
         }
         else {
-            userStores = await getUserStore(user_id, convertedPageNumber);   
+            userStores = await getUserStore(user_id, convertedPageNumber);  
         }
         
         res.status(200).json(userStores);
